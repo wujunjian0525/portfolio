8b9185dab3cd5215722ff397d1dfab2520b401db
mskj-linzilun <linzilun@mskj.com>
2021-09-18 16:21:44 +0800

    1.修改文章模板；2.实现暗黑模式切换；3.修复部分bbug

--- themes/cupertino/source/js/main.js
+++ themes/cupertino/source/js/main.js
@@ -1,55 +1,58 @@
 (() => {
-    var navEl = document.getElementById('theme-nav');
-    navEl.addEventListener('click', (e) => {
-        if (window.innerWidth <= 600) {
-            if (navEl.classList.contains('open')) {
-                navEl.style.height = ''
-            } else {
-                navEl.style.height = 48 + document.querySelector('#theme-nav .nav-items').clientHeight + 'px'
-            }
-            navEl.classList.toggle('open')
-        } else {
-            if (navEl.classList.contains('open')) {
-                navEl.style.height = ''
-                navEl.classList.remove('open')
-            }
-        }
-    })
-    window.addEventListener('resize', (e) => {
-        if (navEl.classList.contains('open')) {
-            navEl.style.height = 48 + document.querySelector('#theme-nav .nav-items').clientHeight + 'px'
-        }
-        if (window.innerWidth > 600) {
-            if (navEl.classList.contains('open')) {
-                navEl.style.height = ''
-                navEl.classList.remove('open')
-            }
-        }
-    })
-
-    if (document.getElementById('theme-color-scheme-toggle')) {
-        var bodyEl = document.body
-        var themeColorSchemeToggleEl = document.getElementById('theme-color-scheme-toggle')
-        var options = themeColorSchemeToggleEl.getElementsByTagName('input')
-
-        if (Cookies.get('color-scheme')) {
-            bodyEl.setAttribute('data-color-scheme', Cookies.get('color-scheme'))
-        }
-
-        for (const option of options) {
-            if (option.value == bodyEl.getAttribute('data-color-scheme')) {
-                option.checked = true
-            }
-            option.addEventListener('change', (ev) => {
-                var value = ev.target.value
-                bodyEl.setAttribute('data-color-scheme', value)
-                Cookies.set('color-scheme', value)
-                for (const o of options) {
-                    if (o.value != value) {
-                        o.checked = false
-                    }
-                }
-            })
-        }
+  var navEl = document.getElementById("theme-nav");
+  navEl.addEventListener("click", (e) => {
+    if (window.innerWidth <= 600) {
+      if (navEl.classList.contains("open")) {
+        navEl.style.height = "";
+      } else {
+        navEl.style.height = 48 + document.querySelector("#theme-nav .nav-items").clientHeight + "px";
+      }
+      navEl.classList.toggle("open");
+    } else {
+      if (navEl.classList.contains("open")) {
+        navEl.style.height = "";
+        navEl.classList.remove("open");
+      }
     }
-})()
+  });
+  
+  window.addEventListener("resize", (e) => {
+    if (navEl.classList.contains("open")) {
+      navEl.style.height = 48 + document.querySelector("#theme-nav .nav-items").clientHeight + "px";
+    }
+    if (window.innerWidth > 600) {
+      if (navEl.classList.contains("open")) {
+        navEl.style.height = "";
+        navEl.classList.remove("open");
+      }
+    }
+  });
+
+
+  // 切换亮暗模式
+  if (document.getElementById("nav-item-light-dark")) {
+    let bodyEl = document.body;
+    let lightDarkEl = document.getElementById("nav-item-light-dark");
+    lightDarkEl.addEventListener("click", (ev) => {
+      const value = bodyEl.getAttribute("data-color-scheme");
+      console.log(value);
+      if (value === 'auto') {
+        // 判断系统默认是否暗黑
+        if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+          bodyEl.setAttribute("data-color-scheme", 'light');
+          Cookies.set("color-scheme", 'light');
+        } else {
+          bodyEl.setAttribute("data-color-scheme", 'dark');
+          Cookies.set("color-scheme", 'dark');
+        }
+      }
+      else if (value == 'dark') {
+        bodyEl.setAttribute("data-color-scheme", 'light');
+        Cookies.set("color-scheme", 'light');
+      } else if (value == 'light') {
+        bodyEl.setAttribute("data-color-scheme", 'dark');
+        Cookies.set("color-scheme", 'dark');
+      }
+    });
+  }
+})();
